961059ff24e604fcffad4bf1fafe666433c2175d
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-04-06 02:48:47 +0200

    commit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+origins = [
+    "https://kitsunesamaix.github.io",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/{url_path:path}")
