32f79d991253b94fc0126da970a75060acb26c4f
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-04-06 02:21:51 +0200

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
@@ -11,3 +12,15 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/{url_path:path}")
+async def proxy(url_path: str):
+    if not url_path:
+        return {"success": False, "errors": ["url path empty"]}
+
+    response = requests.get(f'https://planner.uniud.it/{url_path}')
+    if response.ok:
+        return response.content
+
+    return {"success": False, "errors": ["request failed", f"status code: {response.status_code}"]}
