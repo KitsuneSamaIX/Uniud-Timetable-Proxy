5c8755628e9deebffb2dbd19599e3fa50dc09938
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2022-04-06 02:26:56 +0200

    commit

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ import requests
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
 @app.get("/{url_path:path}")
 async def proxy(url_path: str):
     if not url_path:
